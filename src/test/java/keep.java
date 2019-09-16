4a94eb85b70c926fa7a06d99c040b0e163dfc13b
dmsgid8456 <hyangol77@gmail.com>
2019-09-16 16:57:55 +0900

    Added ClassFile
    Test

--- src/test/java/keep.java
+++
@@ -1,3 +0,0 @@
-public class keep {
-    // delete this class
-}
---
+++ src/test/java/list/MyArrayListTest.java
@@ -0,0 +1,36 @@
+package list;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MyArrayListTest {
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void remove() {
+    }
+
+    @Test
+    public void containsAll() {
+    }
+
+    @Test
+    public void addAll() {
+    }
+
+    @Test
+    public void retainAll() {
+    }
+
+    @Test
+    public void indexOf() {
+    }
+
+    @Test
+    public void lastIndexOf() {
+    }
+}
